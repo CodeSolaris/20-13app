c8d33856da5329982d6ec81e354d689970df54d1
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-12-06 23:34:26 +0200

    * feat: add Age Calculator widget * feat: add Average Speed Calculator widget

---
+++ age_calc.py
@@ -0,0 +1,52 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, \
+    QGridLayout, QLineEdit, QPushButton
+import sys
+from datetime import datetime
+
+
+
+
+class AgeCalculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        grid = QGridLayout()
+
+        self.setWindowTitle("Age Calculator")
+
+        # create widget
+        name_label = QLabel("Name:")
+        self.name_line_edit = QLineEdit()
+
+        date_of_birth_label = QLabel("Date of Birth MM/DD/YYYY: ")
+        self.date_of_birth_line_edit = QLineEdit()
+
+        calculate_button = QPushButton("Calculate")
+        calculate_button.clicked.connect(self.calculate)
+        self.output_label = QLabel("")
+
+        # add widget to grid
+        
+        grid.addWidget(name_label, 0, 0)
+        grid.addWidget(self.name_line_edit, 0, 1)
+        grid.addWidget(date_of_birth_label, 1, 0)
+        grid.addWidget(self.date_of_birth_line_edit, 1, 1)
+        grid.addWidget(calculate_button, 2, 0, 1, 2)
+        grid.addWidget(self.output_label, 3, 0, 1, 2)
+
+        self.setLayout(grid)
+
+    def calculate(self):
+        current_year = datetime.now().year
+        year_of_birth = int(self.date_of_birth_line_edit.text().split("/")[2])
+        age = current_year - year_of_birth
+        self.output_label.setText(f"{self.name_line_edit.text()} is {age} years old.")
+
+
+
+app = QApplication(sys.argv)
+
+calculator = AgeCalculator()
+
+calculator.show()
+
+sys.exit(app.exec())
---
+++ av_speed.py
@@ -0,0 +1,56 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, \
+    QGridLayout, QLineEdit, QPushButton, QComboBox
+import sys
+
+class AverageSpeedCalculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Average Speed Calculator")
+
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
+
+        self.distance_label = QLabel("Distance:")
+        self.grid.addWidget(self.distance_label, 0, 0)
+
+        self.distance_input = QLineEdit()
+        self.grid.addWidget(self.distance_input, 0, 1)
+
+        self.combo = QComboBox()
+        self.combo.addItems(['km', 'miles'])
+        self.grid.addWidget(self.combo, 0, 2)
+
+        self.time_label = QLabel("Time (hours):")
+        self.grid.addWidget(self.time_label, 1, 0)
+
+        self.time_input = QLineEdit()
+        self.grid.addWidget(self.time_input, 1, 1)
+
+        self.calculate_button = QPushButton("Calculate")
+        self.calculate_button.clicked.connect(self.calculate_average_speed)
+        self.grid.addWidget(self.calculate_button, 2, 0, 1, 3)
+
+        # Define the label for average speed
+        self.average_speed_label = QLabel("Average Speed: N/A")
+        self.grid.addWidget(self.average_speed_label, 3, 0, 1, 3)
+
+    def calculate_average_speed(self):
+        try:
+            distance = float(self.distance_input.text())
+            time = float(self.time_input.text())
+
+            if time == 0:
+                self.average_speed_label.setText("Time can't be zero")
+                return
+
+            speed = distance / time
+            unit = self.combo.currentText()
+
+            self.average_speed_label.setText(f"Average Speed: {speed:.2f} {unit}/h")
+        except ValueError:
+            self.average_speed_label.setText("Please enter valid numbers for distance and time.")
+
+app = QApplication(sys.argv)
+calculator = AverageSpeedCalculator()
+calculator.show()
+sys.exit(app.exec())
